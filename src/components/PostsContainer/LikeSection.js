ccbaafeab8ae9685ec42acb39c360f098d34b7d3
Owlspec3086 <ethical.worker3@gmail.com>
2020-05-12 20:08:21 -0400

    Flex done

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -6,6 +6,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const LikeSection = props => {
+  console.log(props);
+  const clickHandler = () => {
+    props.setLike(props.like + 1);
+  };
   return (
     <div>
       <div
@@ -13,13 +17,13 @@ const LikeSection = props => {
         key="likes-icons-container"
       >
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon icon={faHeart} onClick={clickHandler}  />
         </div>
         <div className="like-section-wrapper">
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className="like-number">27 likes</p>
+      <p className="like-number">{props.like} likes</p>
     </div>
   )
 };
